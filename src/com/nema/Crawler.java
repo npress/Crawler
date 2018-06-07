5dc140bac6dcfc773c264148642c77e87c616b0e
npress <npress@Nemas-iMac-3.local>
2018-06-07 00:42:40 -0700

    Initial commit

---
+++ src/com/nema/Crawler.java
@@ -0,0 +1,141 @@
+package com.nema;
+
+import org.apache.commons.io.IOUtils;
+import org.json.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import javax.net.ssl.HttpsURLConnection;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.*;
+
+public class Crawler {
+    protected List<String> linksToVisit = new ArrayList<>();
+    protected Set<String> queued = new HashSet<>();
+    protected Set<String> visited = new HashSet<>();
+    protected static long MAX_PAGES_TO_CRAWL = 10000;
+    protected static final String REMOTE_JSON = "https://raw.githubusercontent.com/OnAssignment/compass-interview/master/data.json";
+    protected long numRequests;
+    protected long numFailed;
+    protected long numSuccess;
+    public boolean crawlPage(String strUrl)
+    {
+        BufferedReader br = null;
+        HttpsURLConnection connection = null;
+        try
+        {
+            URL url = new URL(strUrl);
+            connection = (HttpsURLConnection)url.openConnection();
+            numRequests++;
+            if(connection.getResponseCode() == 200) // 200 is the HTTP OK status code
+            {
+                System.out.println("Successfully connected to " + url);
+            }
+            else{
+                System.out.println("Failed connecting to " + url +"\n\tStatus Code:"
+                        +connection.getResponseCode());
+                return false;
+            }
+
+            if(!connection.getContentType().contains("text/html"))
+            {
+                System.out.println("\tNot able to parse content type");
+                return false;
+            }
+            br =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String input;
+            while ((input = br.readLine()) != null){
+                sb.append(input.toLowerCase());
+            }
+            br.close();
+            connection.disconnect();
+            String baseUri = url.getProtocol()+"://"+url.getHost();
+            Document htmlDocument = Jsoup.parse(sb.toString(), baseUri);
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            String absUrl;
+            for(Element link : linksOnPage)
+            {
+                absUrl = link.absUrl("href");
+                if(!visited.contains(absUrl)  && !queued.contains(absUrl)){
+                    linksToVisit.add(absUrl);
+                    queued.add(absUrl);
+                }
+
+            }
+            return true;
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("We were not successful in our HTTP request");
+            return false;
+        }
+        finally{
+            if(connection!=null)
+                connection.disconnect();
+            if (br != null)
+                try {
+                        br.close();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    public void crawl(){
+        boolean successful;
+        //iterator = linksToVisit.listIterator();
+        while(visited.size()< MAX_PAGES_TO_CRAWL && !linksToVisit.isEmpty()) {
+                String nextURL = linksToVisit.remove(0);
+                if(!visited.contains(nextURL)){
+                    successful = crawlPage(nextURL);
+                    if(successful)
+                        numSuccess++;
+                    else
+                        numFailed++;
+                    visited.add(nextURL);
+
+                }
+        }
+
+
+
+        if(visited.size()> MAX_PAGES_TO_CRAWL)
+        {
+            System.out.println("Exceeded maximum number of pages to crawl");
+        }
+        System.out.println("total number of http requests performed: "+numRequests+"\n" +
+                "total number of successful requests: "+ numSuccess +"\n" +
+                "total number of failed requests: "+ numFailed +"\n");
+
+    }
+    public Crawler(){
+        URL url = null;
+        try {
+            url = new URL(REMOTE_JSON);
+            JSONObject json = new JSONObject(IOUtils.toString(url, Charset.forName("UTF-8")));
+            JSONArray arr = json.getJSONArray("links");
+            for(int i=0; i<arr.length(); i++){
+                linksToVisit.add(arr.getString(i));
+                queued.add(arr.getString(i));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        if(args.length>0)
+            MAX_PAGES_TO_CRAWL = Long.parseLong(args[0]);
+        Crawler c = new Crawler();
+        c.crawl();
+    }
+}
